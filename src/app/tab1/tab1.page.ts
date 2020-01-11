952fbfb8ff3e40c971c6e6dffbaeaa0b29401ae9
YhzyY <huangzi_yi96111@163.com>
2020-01-10 22:32:03 -0500

    add attack submittion form

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -9,4 +9,22 @@ export class Tab1Page {
 
   constructor() {}
 
+  public event = {
+    EventDate: new Date().toDateString(),
+    EventTime: new Date().toTimeString(),
+    EventLoc: 'inside'
+  }
+
+
+  clearData() {
+    this.event.EventDate = new Date().toDateString();
+    this.event.EventTime = new Date().toTimeString();
+    this.event.EventLoc = 'inside';
+  }
+
+  submitData() {
+    console.log(
+        "EventDate : " + this.event.EventDate + ", EventTime : " + this.event.EventTime + ", EventLoc : " + this.event.EventLoc
+    )
+  }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -14,7 +14,6 @@ export class Tab3Page {
   isSignedUp = false;
   PatientName: string;
   constructor(private uniqueDeviceID: UniqueDeviceID) {}
-  // constructor(private uniqueDeviceID: UniqueDeviceID, private http: HttpClient) {}
 
   showuuid(): void{
     // console.info(device.uuid);
@@ -22,34 +21,5 @@ export class Tab3Page {
         .then((uuid: any) => {console.log(uuid); this.DeviceID = uuid})
         .catch((error: any) => console.log(error));
   }
-
-  // login(): void{
-  //   try {
-  //     this.checkPatient(this.DeviceID).subscribe(data => {this.PatientName = data; console.log(this.PatientName);});
-  //   }catch (e) {
-  //     console.log(e);
-  //   }
-  // }
-  //
-  // checkPatient(DeviceID) {
-  //   return this.http.get<string>(
-  //     'https://stormy-dawn-15351.herokuapp.com/participantName?' +
-  //     'uuid=' + DeviceID,
-  //     {responseType: 'text' as 'json' });
-  // }
-
- // signup(newName: string, uuid: string): void {
- //   console.log('signup : ' + newName + ' and ' + newPassword);
- //   this.addClinician(newName, newPassword).subscribe(result => this.postFeedback = result);
- //   console.log(this.postFeedback);
- // }
-
- //
- //   addClinician(newName: string, newPassword: number) {
- //     return this.http.post<string>(
- //       'https://stormy-dawn-15351.herokuapp.com/newClinician?userName=' + newName + '&password=' + newPassword,
- //       null,
- //       {responseType: 'text' as 'json' });
- //   }
 
 }
