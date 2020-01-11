398f23ad976b18808c4d5fef6bc06b0e371ea7e4
YhzyY <huangzi_yi96111@163.com>
2020-01-10 19:56:40 -0500

    finish identification

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UniqueDeviceID } from '@ionic-native/unique-device-id/ngx';
+import {HttpHeaders, HttpParams, HttpResponse} from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +10,46 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  DeviceID: string;
+  isSignedUp = false;
+  PatientName: string;
+  constructor(private uniqueDeviceID: UniqueDeviceID) {}
+  // constructor(private uniqueDeviceID: UniqueDeviceID, private http: HttpClient) {}
+
+  showuuid(): void{
+    // console.info(device.uuid);
+    this.uniqueDeviceID.get()
+        .then((uuid: any) => {console.log(uuid); this.DeviceID = uuid})
+        .catch((error: any) => console.log(error));
+  }
+
+  // login(): void{
+  //   try {
+  //     this.checkPatient(this.DeviceID).subscribe(data => {this.PatientName = data; console.log(this.PatientName);});
+  //   }catch (e) {
+  //     console.log(e);
+  //   }
+  // }
+  //
+  // checkPatient(DeviceID) {
+  //   return this.http.get<string>(
+  //     'https://stormy-dawn-15351.herokuapp.com/participantName?' +
+  //     'uuid=' + DeviceID,
+  //     {responseType: 'text' as 'json' });
+  // }
+
+ // signup(newName: string, uuid: string): void {
+ //   console.log('signup : ' + newName + ' and ' + newPassword);
+ //   this.addClinician(newName, newPassword).subscribe(result => this.postFeedback = result);
+ //   console.log(this.postFeedback);
+ // }
+
+ //
+ //   addClinician(newName: string, newPassword: number) {
+ //     return this.http.post<string>(
+ //       'https://stormy-dawn-15351.herokuapp.com/newClinician?userName=' + newName + '&password=' + newPassword,
+ //       null,
+ //       {responseType: 'text' as 'json' });
+ //   }
 
 }
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,101 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {UniqueDeviceID} from "@ionic-native/unique-device-id/ngx";
+import {AlertController} from "@ionic/angular";
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage {
+export class TabsPage implements OnInit {
+  private DeviceID: string;
+  private PatientName: string;
+  private postFeedback: string;
+  private temp: string;
 
-  constructor() {}
+  constructor(private http: HttpClient, public alertCtrl: AlertController) {}
+  // constructor(private uniqueDeviceID: UniqueDeviceID, private http: HttpClient) {}
+
+  login(): void{
+
+    try {
+      this.checkPatient(this.DeviceID).subscribe(
+          data => {this.PatientName = data; console.log(this.PatientName);},
+          async error => {
+            console.log('HttpClient get checkPatient error');
+            this.showPrompt();
+
+          }
+      );
+    }catch (e) {
+      console.log(e);
+    }
+  }
+
+  // getuuid(): void{
+  //   // console.info(device.uuid);
+  //   this.uniqueDeviceID.get()
+  //       .then((uuid: any) => {console.log(uuid); this.DeviceID = uuid})
+  //       .catch((error: any) => console.log(error));
+  // }
+
+  checkPatient(DeviceID) {
+    return this.http.get<string>(
+        'https://stormy-dawn-15351.herokuapp.com/participantName?' +
+        'uuid=' + DeviceID,
+        {responseType: 'text' as 'json' });
+  }
+
+  async showPrompt() {
+    const prompt = await this.alertCtrl.create({
+      header: 'Login',
+      message: "Enter a username to use our system",
+      inputs: [
+        {
+          name: 'username',
+          type: 'text',
+          placeholder: 'username'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'OK',
+          handler: data => {
+            this.PatientName = JSON.parse(JSON.stringify(data)).username;
+            console.log('OK clicked, PatientName = ', this.PatientName);
+          }
+        }
+      ]
+    });
+    prompt.onDidDismiss().then(() => this.signup(this.PatientName, this.DeviceID));
+    await prompt.present();
+  }
+
+  signup(newName: string, uuid: string): void {
+    console.log('signup : ' + newName + ' and ' + uuid);
+    this.addUser(newName, uuid).subscribe(result => {this.postFeedback = result; console.log(this.postFeedback)});
+  }
+
+
+  addUser(newName: string,  uuid: string) {
+      return this.http.post<string>(
+        'https://stormy-dawn-15351.herokuapp.com/newUser?name=' + newName + '&uuid=' + uuid,
+        null,
+        {responseType: 'text' as 'json' });
+    }
+
+  ngOnInit(): void {
+    // this.getuuid();
+    this.DeviceID = '33clhl';
+    this.login();
+  }
 
 }
