53b5bd5755d0bf895f844835c676cb6b274cac0e
YhzyY <huangzi_yi96111@163.com>
2020-01-11 22:50:23 -0500

    save new attack (UTC time

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -21,18 +21,29 @@ export class Tab1Page {
 
   clearData() {
     this.event.EventDate = new Date().toLocaleDateString();
-    // this.event.EventTime = new Date().getHours() + ":" + new Date().getMinutes();
     this.event.EventTime = new Date().getHours() + ":" + (new Date().getMinutes()<10?'0':'') + new Date().getMinutes();
     this.event.EventLoc = 'inside';
   }
 
   submitData() {
     this.event.EventDate = new Date(this.event.EventDate).toLocaleDateString();
+
+    let newday = new Date(this.event.EventDate + ' ' + this.event.EventTime);
+    let UTCdate = newday.getUTCFullYear() + '/' + (newday.getUTCMonth() + 1) + '/' + newday.getUTCDate();
+    let UTCtime = ((newday.getUTCHours()<10?'0':'') + newday.getUTCHours() )+ ':' + ((newday.getUTCMinutes()<10?'0':'') + newday.getUTCMinutes());
     console.log(
-        "EventDate : " + this.event.EventDate  + ", EventTime : " + this.event.EventTime + ", EventLoc : " + this.event.EventLoc + ", uuid : " + TabsPage.deviceId
+        newday,
+        "\n",
+        "LocalDate : " + this.event.EventDate  + ", LocalTime : " + this.event.EventTime + ", EventLoc : " + this.event.EventLoc + ", uuid : " + TabsPage.deviceId,
+        "\n",
+        "UTC : EventDate : " + UTCdate  + ", EventTime : " + UTCtime + ", EventLoc : " + this.event.EventLoc + ", uuid : " + TabsPage.deviceId,
     )
-    this.submitRequest(this.event.EventDate, this.event.EventTime, this.event.EventLoc)
-        .subscribe(result => {console.log(result)});
+
+    this.submitRequest(UTCdate, UTCtime, this.event.EventLoc)
+        .subscribe(result => {
+          console.log(result);
+          this.submitFeedback(result)
+        });
   }
 
   submitRequest(EventDate: string, EventTime: string, EventLoc: string) {
@@ -44,5 +55,14 @@ export class Tab1Page {
         '&uuid=' + TabsPage.deviceId,
         null,
         {responseType: 'text' as 'json' });
+  }
+
+  submitFeedback(result: string){
+    if(result == 'new attack is added'){
+      console.log('result check true successful')
+    }else{
+      console.log('result check false successful')
+    }
+
   }
 }
